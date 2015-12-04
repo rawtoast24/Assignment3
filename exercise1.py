2fdfab776c36fe4df8210c704570e0beca96c112
rawtoast24 <anirudh24@gmail.com>
2015-12-03 20:31:47 -0500

    Exercise 1 should be complete; testing required

--- exercise1.py
+++ exercise1.py
@@ -37,23 +37,23 @@ class UnknownAttributeException(Exception):
     """
     pass
 
-EMPLOYEES = [["Surname", "FirstName", "Age", "Salary"],
-             ["Smith", "Mary", 25, 2000],
-             ["Black", "Lucy", 40, 3000],
-             ["Verdi", "Nico", 36, 4500],
-             ["Smith", "Mark", 40, 3900]]
-
-
-def filter_employees(row):
-    """
-    Check if employee represented by row
-    is AT LEAST 30 years old and makes
-    MORE THAN 3500.
-    :param row: A List in the format:
-        [{Surname}, {FirstName}, {Age}, {Salary}]
-    :return: True if the row satisfies the condition.
-    """
-    return row[-2] >= 30 and row[-1] > 3500
+# EMPLOYEES = [["Surname", "FirstName", "Age", "Salary"],
+#              ["Smith", "Mary", 25, 2000],
+#              ["Black", "Lucy", 40, 3000],
+#              ["Verdi", "Nico", 36, 4500],
+#              ["Smith", "Mark", 40, 3900]]
+#
+#
+# def filter_employees(row):
+#     """
+#     Check if employee represented by row
+#     is AT LEAST 30 years old and makes
+#     MORE THAN 3500.
+#     :param row: A List in the format:
+#         [{Surname}, {FirstName}, {Age}, {Salary}]
+#     :return: True if the row satisfies the condition.
+#     """
+#     return row[-2] >= 30 and row[-1] > 3500
 
 
 def selection(t1, f):
@@ -71,6 +71,7 @@ def selection(t1, f):
     """
     i = 1
     result = []
+    pass
     result.append(t1[0])
     while i < len(t1):
         if f(t1[i]):
@@ -81,13 +82,13 @@ def selection(t1, f):
     return result
 
 
-GRADUATES = [["Number", "Surname", "Age"],
-             [7274, "Robinson", 37],
-             [7432, "O'Malley", 39],
-             [9824, "Darkes", 38]]
+# GRADUATES = [["Number", "Surname", "Age"],
+#              [7274, "Robinson", 37],
+#              ["Surname", "O'Malley", 39],
+#              [9824, "Darkes", 38]]
 
 
-def projection(table, attributelist):
+def projection(t, r):
     """
     Perform projection operation on table t
     using the attributes subset r.
@@ -106,20 +107,21 @@ def projection(table, attributelist):
     k = 0
     result = []
 
-    while i < len(attributelist):
-        while k < len(table[j]):
-            if attributelist[i] == table[j][k]:
-                while j < len(table):
-                    result.append([table[j][k]])
+    while i < len(r):
+        while k < len(t[j]):
+            if r[i] == t[j][k]:
+                while j < len(t):
+                    result.append([t[j][k]])
                     j += 1
             j = 0
             k += 1
         k = 0
         i += 1
 
-    return result
+    if len(result) == 0:
+        raise UnknownAttributeException
 
-print projection(GRADUATES, ["Surname"])
+    return result
 
 
 def cross_product(t1, t2):
@@ -132,8 +134,19 @@ def cross_product(t1, t2):
     [["A", "B", "C", "D"], [1, 2, 5, 6], [3, 4, 5, 6]]
 
     """
-    t1 = 0
-    t2 = 0
+    i = 1
+    j = 1
+    result = []
+    pass
 
-    result = t1 + t2
+    result = [t1[0] + t2[0]]
+    while i < len(t1):
+        while j < len(t2):
+            result.append(t1[i]+t2[j])
+            j += 1
+        j = 1
+        i += 1
+    if len(result) == 1:
+        result = None
+
     return result
